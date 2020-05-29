d8d2bfe5101714e4893d26e901644bbd01773489
gaowei <623255490@qq.com>
2020-05-29 09:49:32 +0800

    dtc code parsing fix

--- LTSupportAutomotive/LTOBD2ProtocolISO15765_4.m
+++ LTSupportAutomotive/LTOBD2ProtocolISO15765_4.m
@@ -85,10 +85,10 @@
 		NSString *first03 = lines.firstObject;
 		NSRange range = [first03 rangeOfString:@"43"];
 		if (range.location != NSNotFound && (range.location + range.length + 2) <= first03.length){
-			NSInteger dtcCount = [[first03 substringWithRange:NSMakeRange(range.location + range.length, 2)] integerValue];
-			if (dtcCount <= 0) {
-				return [NSDictionary dictionaryWithDictionary:md];
-			}
+//			NSInteger dtcCount = [[first03 substringWithRange:NSMakeRange(range.location + range.length, 2)] integerValue];
+//			if (dtcCount <= 0) {
+//				return [NSDictionary dictionaryWithDictionary:md];
+//			}
 		}else{
 			return [NSDictionary dictionaryWithDictionary:md];
 		}
@@ -155,7 +155,7 @@
 			NSArray<NSNumber *> *payload = [bytesInLine subarrayWithRange:payloadRange];
 			[resultForSource appendPayloadBytes:payload];
 		}
-		if ([command isEqualToString:@"03"] && isFirstFrameOfMultiple){
+		if ([command isEqualToString:@"03"] && (isSingleFrame || isFirstFrameOfMultiple)){
 			[resultForSource removeAtIndex:0];
 		}
     }
